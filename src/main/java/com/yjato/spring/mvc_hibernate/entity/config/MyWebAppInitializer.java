a05dd128ff0232c3646a587d555f9e0bec4a983c
yjato <155980319+yjato@users.noreply.github.com>
2024-04-20 19:59:28 +0700

    Add files via upload

---
+++ src/main/java/com/yjato/spring/mvc_hibernate/entity/config/MyWebAppInitializer.java
@@ -0,0 +1,20 @@
+package com.yjato.spring.mvc_hibernate.entity.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
